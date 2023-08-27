a776d570e7f9088d212ce5e127d8acbdc0712cd2
Alex Kovacs <axkovacs27@gmail.com>
2023-08-27 13:35:01 +0200

    commit

--- src/auth/dto/register-user.dto.ts
+++ src/auth/dto/register-user.dto.ts
@@ -5,19 +5,10 @@ export class RegisterUserDto {
     @IsEmail()
     email: string;
 
-    @MinLength(6)
-    password: string;
-
     @IsString()
     name: string;
 
-    @IsString()
-    surname: string;
-
-    @IsString()
-    birthdate: string;
-
-    @IsString()
-    gender: string;
+    @MinLength(6)
+    password: string;
 
 }
